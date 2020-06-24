e4869d6ca27953071a02f909c2d445e2e0b4d7d1
matt-stanford <mjfstanford@gmail.com>
2020-06-24 20:47:58 +0100

    added modals

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,36 @@
 const scroll = new SmoothScroll('a[href*="#"]', {
     speed: 500
 });
+
+const modals = document.querySelectorAll('.modal');
+const modalBtns = document.querySelectorAll('#modalBtn');
+const closeBtns = document.querySelectorAll('.closeBtn');
+
+modalBtns.forEach(btn => {
+    btn.addEventListener('click', function() {
+        let jobId = this.dataset.jobid
+        console.log(jobId)
+        let modal = document.getElementById('jobModal' + jobId);
+        console.log(modal)
+        modal.style.display = 'block'
+    });
+});
+
+closeBtns.forEach(btn => {
+    btn.addEventListener('click', function() {
+        let jobId = this.dataset.jobid
+        console.log(jobId)
+        let modal = document.getElementById('jobModal' + jobId);
+        console.log(modal)
+        modal.style.display = 'none'
+    });
+});
+
+modals.forEach(modal => {
+    window.addEventListener('click', function(e) {
+        if(e.target == modal) {
+            modal.style.display = 'none'
+        }
+    });
+});
+
