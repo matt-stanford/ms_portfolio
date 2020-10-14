e7eadec39470f048e17fff764c6794d7d2e1ca21
matt-stanford <mjfstanford@gmail.com>
2020-10-14 10:10:13 +0100

    Set Debug Value

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('MS_PORTFOLIO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') ==  'True')
 
 ALLOWED_HOSTS = [
     'mattstanforddev.herokuapp.com',
